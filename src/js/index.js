a2351ceafe6aa00000e072bf71cc15cc58087097
ORFJunior <146041038+ORFJunior@users.noreply.github.com>
2024-06-01 12:38:57 -0300

    Add files via upload

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,26 @@
 
 
 const botao_mostrar_projetos = document.querySelector('.btn-mostrar-projetos');
-const projetos_inativos = document.querySelectorAll('.projeto:not(.ativo)')
+const projetos_inativos = document.querySelectorAll('.projeto:not(.ativo)');
+const botao_ocultar_projetos = document.querySelector('.btn-ocultar-projetos');
 
 botao_mostrar_projetos.addEventListener('click', () => {
     mostrar_projetos();
 
     esconder_botao();
 });
+
+
+botao_ocultar_projetos.addEventListener('click', () =>{
+    projetos_inativos.forEach(projeto_inativo => {
+        projeto_inativo.classList.remove('ativo');
+    });
+
+
+    botao_ocultar_projetos.classList.remove('adicionar');
+    botao_mostrar_projetos.classList.remove('remover');
+
+})
 
 
 function esconder_botao(){
@@ -17,5 +30,6 @@ function esconder_botao(){
 function mostrar_projetos(){
     projetos_inativos.forEach(projeto_inativo => {
         projeto_inativo.classList.add('ativo');
+        botao_ocultar_projetos.classList.add('adicionar');
     });
 }
